842be9193e78e479143a807e524fa3a901a9e9e2
Axel_Mastroianni <a.mastroianni@studenti.unibs.it>
2022-05-19 14:40:01 +0200

    Modified configuration file

--- api-test/docusaurus.config.js
+++ api-test/docusaurus.config.js
@@ -69,9 +69,8 @@ const config = {
                         position: "left",
                         label: "Documentation",
                     },
-                    { to: "/blog", label: "Blog", position: "left" },
                     {
-                        href: "https://github.com/facebook/docusaurus",
+                        href: "https://github.com/Kodaku/APITestDocumentation/tree/main/api-test",
                         label: "GitHub",
                         position: "right",
                     },
@@ -85,7 +84,7 @@ const config = {
                         items: [
                             {
                                 label: "Tutorial",
-                                to: "/docs/intro",
+                                to: "/docs/category/introduction",
                             },
                         ],
                     },
@@ -110,17 +109,13 @@ const config = {
                         title: "More",
                         items: [
                             {
-                                label: "Blog",
-                                to: "/blog",
-                            },
-                            {
                                 label: "GitHub",
-                                href: "https://github.com/facebook/docusaurus",
+                                href: "https://github.com/Kodaku/APITestDocumentation/tree/main/api-test",
                             },
                         ],
                     },
                 ],
-                copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+                copyright: `Copyright © ${new Date().getFullYear()} API Test, Inc. Built with Docusaurus.`,
             },
             prism: {
                 theme: lightCodeTheme,
